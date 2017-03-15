43607250c183d75ac06cefd8c7d009f8bbb58961
testing <testing@IN09PC17-PC.corp.logitech.com>
2017-03-15 17:03:21 +0530

    calculation

--- src/components/AthletePreview.js
+++ src/components/AthletePreview.js
@@ -4,13 +4,26 @@ import React from 'react';
 import { Link } from 'react-router';
 
 export default class AthletePreview extends React.Component {
+  calculateTotalPoints()
+  {
+    var totalpoints =0;
+    var myGameRows =this.props.medals;
+    if(myGameRows.length>0)
+    {
+      for(var s=0;s<myGameRows.length; s++)
+      {
+       totalpoints = totalpoints + parseInt(myGameRows[s].category);
+      }
+    }
+    return totalpoints;
+  }
   render() {
     return (
       <Link to={`/athlete/${this.props.id}`}>
         <div className="athlete-preview">
           <img src={`img/${this.props.image}`}/>
           <h2 className="name">{this.props.name}</h2>
-          <span className="medals-count"><img src="/img/medal.png"/> {this.props.medals.length}</span>
+          <span className="medals-count"><img src="/img/medal.png"/> {this.calculateTotalPoints()}</span>
         </div>
       </Link>
     );
