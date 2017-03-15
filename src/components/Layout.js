e25e5c3cb9cb816c7e05298fd7af9c8037fd7bda
testing <testing@IN09PC17-PC.corp.logitech.com>
2017-03-15 13:21:45 +0530

    folder

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,22 +4,31 @@ import React from 'react';
 import { Link } from 'react-router';
 
 export default class Layout extends React.Component {
+  getDate()
+  {
+   return new Date().toLocaleDateString();
+  }
   render() {
     return (
       <div className="app-container">
         <header>
+        <div className="datediv">
+        <label>{this.getDate()}</label>
+        </div>
+        <div>
           <Link to="/">
-            <img className="logo" src="/img/logo-judo-heroes.png"/>
+            <label className="logo logoheading">Logi Heroes</label>
           </Link>
+          </div>
         </header>
         <div className="app-content">{this.props.children}</div>
         <footer>
           <p>
-            This is a demo app to showcase universal rendering and routing with <strong>React</strong> and <strong>Express</strong>.
+           Know your clan's positions and keep up the momentum <strong>I love the winning, I can take the losing, but most of all I Love to play. </strong> and <strong>Participate to win gifts</strong>.
           </p>
 
           <p>
-            Do you want to know more about Node.js and Universal JavaScript? <strong>Checkout <a href="https://nodejsdesignpatterns.com">Node.js Design Patterns</a></strong>.
+            Do you want to know more about Logitech Olympics? <strong>Checkout <a href="https://nodejsdesignpatterns.com">LARC</a></strong>.
           </p>
 
           <p>
