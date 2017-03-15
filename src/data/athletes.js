40c543695978815c731f9df2025a3048e9aa74c2
testing <testing@IN09PC17-PC.corp.logitech.com>
2017-03-15 14:22:36 +0530

    changes in display order

--- src/data/athletes.js
+++ src/data/athletes.js
@@ -1,33 +1,7 @@
 "use strict";
 
 const athletes = [
-  {
-    'id': 'clan-d',
-    'name': 'CLAN D',
-    'country': 'cu',
-    'birth': '1973',
-    'image': 'gryffindor.jpg',
-    'cover': 'gryfindor-banner.jpg',
-    'link': 'https://en.wikipedia.org/wiki/Driulis_González',
-    'medals': [
-      { 'year': '2017', 'type': 'B', 'city': 'chess', 'event': 'Olympic Games', 'category': '-57kg' },
-      { 'year': '2017', 'type': 'B', 'city': 'carrom', 'event': 'World Championships', 'category': '-57kg' },
-      { 'year': '2017', 'type': 'G', 'city': 'cards', 'event': 'World Championships', 'category': '-57kg' },
-      { 'year': '2017', 'type': 'G', 'city': 'Mar del Plata', 'event': 'Pan American Games', 'category': '-57kg' },
-      { 'year': '1996', 'type': 'G', 'city': 'Atlanta', 'event': 'Olympic Games', 'category': '-57kg' },
-      { 'year': '1997', 'type': 'S', 'city': 'Osaka', 'event': 'World Championships', 'category': '-57kg' },
-      { 'year': '1999', 'type': 'G', 'city': 'Birmingham', 'event': 'World Championships', 'category': '-57kg' },
-      { 'year': '2000', 'type': 'S', 'city': 'Sydney', 'event': 'Olympic Games', 'category': '-57kg' },
-      { 'year': '2003', 'type': 'G', 'city': 'S Domingo', 'event': 'Pan American Games', 'category': '-63kg' },
-      { 'year': '2003', 'type': 'S', 'city': 'Osaka', 'event': 'World Championships', 'category': '-63kg' },
-      { 'year': '2004', 'type': 'B', 'city': 'Athens', 'event': 'Olympic Games', 'category': '-63kg' },
-      { 'year': '2005', 'type': 'B', 'city': 'Cairo', 'event': 'World Championships', 'category': '-63kg' },
-      { 'year': '2006', 'type': 'G', 'city': 'Cartagena', 'event': 'Central American and Caribbean Games', 'category': '-63kg' },
-      { 'year': '2006', 'type': 'G', 'city': 'Cartagena', 'event': 'Central American and Caribbean Games', 'category': 'Tema' },
-      { 'year': '2007', 'type': 'G', 'city': 'Rio de Janeiro', 'event': 'Pan American Games', 'category': '-63kg' },
-      { 'year': '2007', 'type': 'G', 'city': 'Rio de Janeiro', 'event': 'World Championships', 'category': '-63kg' },
-    ],
-  },
+ 
   {
     'id': 'clan-a',
     'name': 'CLAN A',
@@ -98,6 +72,33 @@ const athletes = [
       { 'year': '2004', 'type': 'G', 'city': 'Athens', 'event': 'Olympic Games', 'category': '-48kg' },
       { 'year': '2007', 'type': 'G', 'city': 'Rio de Janeiro', 'event': 'World Championships', 'category': '-48kg' },
       { 'year': '2008', 'type': 'B', 'city': 'Beijing', 'event': 'Olympic Games', 'category': '-48kg' },
+    ],
+  },
+   {
+    'id': 'clan-d',
+    'name': 'CLAN D',
+    'country': 'cu',
+    'birth': '1973',
+    'image': 'gryffindor.jpg',
+    'cover': 'gryfindor-banner.jpg',
+    'link': 'https://en.wikipedia.org/wiki/Driulis_González',
+    'medals': [
+      { 'year': '2017', 'type': 'B', 'city': 'chess', 'event': 'Olympic Games', 'category': '-57kg' },
+      { 'year': '2017', 'type': 'B', 'city': 'carrom', 'event': 'World Championships', 'category': '-57kg' },
+      { 'year': '2017', 'type': 'G', 'city': 'cards', 'event': 'World Championships', 'category': '-57kg' },
+      { 'year': '2017', 'type': 'G', 'city': 'Mar del Plata', 'event': 'Pan American Games', 'category': '-57kg' },
+      { 'year': '1996', 'type': 'G', 'city': 'Atlanta', 'event': 'Olympic Games', 'category': '-57kg' },
+      { 'year': '1997', 'type': 'S', 'city': 'Osaka', 'event': 'World Championships', 'category': '-57kg' },
+      { 'year': '1999', 'type': 'G', 'city': 'Birmingham', 'event': 'World Championships', 'category': '-57kg' },
+      { 'year': '2000', 'type': 'S', 'city': 'Sydney', 'event': 'Olympic Games', 'category': '-57kg' },
+      { 'year': '2003', 'type': 'G', 'city': 'S Domingo', 'event': 'Pan American Games', 'category': '-63kg' },
+      { 'year': '2003', 'type': 'S', 'city': 'Osaka', 'event': 'World Championships', 'category': '-63kg' },
+      { 'year': '2004', 'type': 'B', 'city': 'Athens', 'event': 'Olympic Games', 'category': '-63kg' },
+      { 'year': '2005', 'type': 'B', 'city': 'Cairo', 'event': 'World Championships', 'category': '-63kg' },
+      { 'year': '2006', 'type': 'G', 'city': 'Cartagena', 'event': 'Central American and Caribbean Games', 'category': '-63kg' },
+      { 'year': '2006', 'type': 'G', 'city': 'Cartagena', 'event': 'Central American and Caribbean Games', 'category': 'Tema' },
+      { 'year': '2007', 'type': 'G', 'city': 'Rio de Janeiro', 'event': 'Pan American Games', 'category': '-63kg' },
+      { 'year': '2007', 'type': 'G', 'city': 'Rio de Janeiro', 'event': 'World Championships', 'category': '-63kg' },
     ],
   }
 ];
